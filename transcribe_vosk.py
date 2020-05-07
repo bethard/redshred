e61fb91ba917275246aebc15daa70538b035a089
Steven Bethard <bethard@email.arizona.edu>
2020-05-07 15:27:22 -0700

    Improves vosk code

--- transcribe_vosk.py
+++ transcribe_vosk.py
@@ -2,23 +2,20 @@ from vosk import Model, KaldiRecognizer
 import sys
 import json
 import os
+import wave
 
 model = Model("vosk-model-en-us-aspire-0.2")
 
 for path in sys.argv[1:]:
-    rec = KaldiRecognizer(model, 44100)
-
     print(path)
-    wf = open(path, "rb")
-    wf.read(44) # skip header
 
+    wf = wave.open(path, "rb")
+    rec = KaldiRecognizer(model, wf.getframerate())
     while True:
-        data = wf.read(4000)
+        data = wf.readframes(1000)
         if len(data) == 0:
             break
-        if rec.AcceptWaveform(data):
-            res = json.loads(rec.Result())
-            print(res['text'])
+        rec.AcceptWaveform(data)
 
     res = json.loads(rec.FinalResult())
-    print(res['text'])
+    print(res["text"])
