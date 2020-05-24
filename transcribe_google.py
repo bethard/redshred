7cdc82a8c0a22439b198901270783d30b62ede96
Steven Bethard <steven.bethard@gmail.com>
2020-05-24 16:13:50 -0700

    Adds command line interface. Updates boosted words.

--- transcribe_google.py
+++ transcribe_google.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import argparse
 
 from google.cloud import speech_v1p1beta1 as speech
 from google.protobuf import json_format
@@ -12,38 +13,56 @@ client = speech.SpeechClient()
 
 config = dict(
     language_code="en-US",
-    sample_rate_hertz=44100,
     audio_channel_count=2,
     model="default",
     speech_contexts=[
         dict(
             phrases=[
+                # what Vader gets recognized as
                 "radar",
+                # phrases about following procedures
                 "accordance",
+                "shall",
                 "log",
                 "logging",
                 "out",
-                "man",
-                "number",
                 "task",
                 "step",
+                "steps",
                 "check",
                 "list",
+                "closed",
+                "inspected",
+                "revision",
+                "tack",
+                "warning",
+                "read off",
+                "notes",
+                # phrases about personnel
+                "staff",
+                "sergeant",
+                "man",
+                "number",
+                # phrases about airplane maintenance
                 "side",
                 "nose",
                 "door",
                 "landing",
+                "candidate",
+                "fault",
                 "applying",
-                "on the",
-                "we are",
-                "we have",
-                "revision",
-                "staff",
-                "sergeant",
-                "closed",
-                "inspected",
-                "kansas",
-                "alpha",
+                "prepare",
+                "wrench",
+                "circuits",
+                "leads",
+                "electrical",
+                "bonding",
+                "mating",
+                "relay",
+                "tag",
+                "tagged",
+                "nuts",
+                "studs",
                 "e v s",
                 "h s",
             ],
@@ -51,8 +70,41 @@ config = dict(
         ),
         dict(
             phrases=[
+                # phrases that should be above
+                # but are confused with common words
                 "hour",
                 "dash",
+                "torque",
+                # NATO phonetic alphabet
+                "Alfa",
+                "Bravo",
+                "Charlie",
+                "Delta",
+                "Echo",
+                "Foxtrot",
+                "Golf",
+                "Hotel",
+                "India",
+                "Juliett",
+                "Kilo",
+                "Lima",
+                "Mike",
+                "November",
+                "Oscar",
+                "Papa",
+                "Quebec",
+                "Romeo",
+                # "Sierra", # can't boost this; it sounds too much like zero
+                "Tango",
+                "Uniform",
+                "Victor",
+                "Whiskey",
+                "X-ray",
+                "Yankee",
+                "Zulu",
+                # boost numbers
+                "$OPERAND",
+                "0",
             ],
             boost=5,
         ),
@@ -60,26 +112,44 @@ config = dict(
     #enable_word_time_offsets=True,
 )
 
-wav_dir = os.path.join(root_dir, "REDSHREAD VOICE RECORDINGS")
-json_dir = os.path.join(root_dir, "json")
-if not os.path.exists(json_dir):
-    os.makedirs(json_dir)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("wav_dir")
+    parser.add_argument("json_dir", nargs='?', default="json")
+    parser.add_argument("--expected-dir")
+    parser.add_argument("--expected-encoding", default="utf8")
+    parser.add_argument("--only", dest="patterns", metavar="PATTERN", nargs='+')
+    args = parser.parse_args()
 
-paths = [(dirpath, filename)
-         for dirpath, dirnames, filenames in os.walk(wav_dir)
-         for filename in filenames
-         if filename.endswith(".wav")]
+    if not os.path.exists(args.json_dir):
+        os.makedirs(args.json_dir)
 
-for dirpath, filename in sorted(paths):
-    wav_path = os.path.join(dirpath, filename)
-    print(wav_path)
-    with open(wav_path, "rb") as f:
-        content = f.read()
+    paths = [(dirpath, filename)
+             for dirpath, dirnames, filenames in os.walk(args.wav_dir)
+             for filename in filenames
+             if not args.patterns or any(p in filename for p in args.patterns)
+             if filename.endswith(".wav")]
 
-    audio = dict(content=content)
-    response = client.recognize(config, audio)
-    for result in response.results:
-        print(result.alternatives[0].transcript)
-    json_path = os.path.join(json_dir, filename.replace(".wav", ".json"))
-    with open(json_path, 'w') as json_file:
-        json_file.write(json_format.MessageToJson(response))
+    for dirpath, filename in sorted(paths):
+        wav_path = os.path.join(dirpath, filename)
+        if args.expected_dir:
+            txt_filename = filename.replace(".wav", ".txt")
+            txt_path = os.path.join(args.expected_dir, txt_filename)
+            if os.path.exists(txt_path):
+                with open(txt_path, encoding=args.expected_encoding) as f:
+                    print(filename + " EXPECTED:")
+                    print(f.read())
+
+        with open(wav_path, "rb") as f:
+            content = f.read()
+
+        audio = dict(content=content)
+        response = client.recognize(config, audio)
+        print(filename + " TRANSCRIBED:")
+        for result in response.results:
+            print(result.alternatives[0].transcript)
+        json_path = os.path.join(args.json_dir, filename.replace(".wav", ".json"))
+        with open(json_path, 'w') as json_file:
+            json_file.write(json_format.MessageToJson(response))
+
+        print()
